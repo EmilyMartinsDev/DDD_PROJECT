60001969ea8e0ae74f0b82b650cd89296cd92b42
emily <emily@Emily>
2024-05-11 15:51:01 -0300

    testes e2e

--- dist/domain/customer/events/handler/CustomerCreatedVersion2Handler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CustomerCreateHandlerVersion2 {
-    handler(event) {
-        console.log("Esse é o segundo console.log do evento: CustomerCreated");
-    }
-}
-exports.default = CustomerCreateHandlerVersion2;
